e88c0721edfe857047d78e089b33db375736d09c
Sivareddy0930 <sivareddy0930@gmail.com>
2024-09-10 23:02:18 +0530

    JPA and Mapping

---
+++ src/main/java/com/projectJpa/SpringDataPractice/SpringDataPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.projectJpa.SpringDataPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectJpa/SpringDataPractice/SpringDataPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectJpa.SpringDataPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
